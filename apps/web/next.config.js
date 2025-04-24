d4048d3f403c7db51dbd67728518e949e884a6e4
hkrishna8124 <hkrishna8124@gmail.com>
2025-04-24 17:22:17 +0530

    fix: enable standalone output for Next.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  transpilePackages: ["./ui"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
 };
 export default nextConfig;
