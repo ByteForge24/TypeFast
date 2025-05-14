163523ed4991c930af9261d84bed3b63b2fdef98
hkrishna8124 <hkrishna8124@gmail.com>
2025-05-14 12:18:24 +0530

    push

--- apps/web/components/multiplayer/join-room.tsx
+++ apps/web/components/multiplayer/join-room.tsx
@@ -96,10 +96,6 @@ const JoinRoom = () => {
         )}
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
               control={form.control}
               name="code"
